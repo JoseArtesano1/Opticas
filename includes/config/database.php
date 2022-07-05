22832b70ceabd07a9eb30bc5975e58d94880e4b8
JoseArtesano1 <jakastrillor@gmail.com>
2022-07-06 01:02:29 +0200

    fin

--- includes/config/database.php
+++ includes/config/database.php
@@ -7,6 +7,7 @@ function conectarDB (): mysqli{
   // $db= new mysqli('127.0.0.1','root','root','optica');
   $db= new mysqli( $_ENV['DB_HOST'],  $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_DB']);
    $db->set_charset("utf8");
+   
     if(!$db){
         echo "Error no se puede conectar";
         exit;  //para evitar mostrar informacion importante
